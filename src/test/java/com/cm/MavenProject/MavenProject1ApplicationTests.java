e140c638a813e1e2978a2f35254e6c90f11ea660
C\DELL <cosimomazzarella1989@gmail.com>
2021-02-18 21:28:47 +0100

    modifica package

---
+++ src/test/java/com/cm/MavenProject/MavenProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cm.MavenProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
